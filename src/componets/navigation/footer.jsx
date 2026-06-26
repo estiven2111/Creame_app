f51e0093720f70248d9c72024d458403b869be03
estiven2111 <estiven.a.martinez@gmail.com>
2026-06-26 12:35:15 -0500

    refactor: actualizar el logo en el componente footer y ajustar el tamaño del contenedor

--- src/componets/navigation/footer.jsx
+++ src/componets/navigation/footer.jsx
@@ -1,12 +1,12 @@
 
-import icon from "../../assets/img/logo_syncroniza.png"
+import icon from "../../assets/img/simErp.png"
 function footer() {
   return (
     <div>
   
 <footer className="bg-white flex justify-end items-center  bottom-0 left-0 right-0 py-6 z-10">
     <label htmlFor="">Power By: </label>
-<div className="lg:w-1/10 md:w-1/10 w-1/15 align-text-bottom ">
+<div className="lg:w-1/15 md:w-1/15 w-2/12 align-text-bottom ">
     <img src={icon} alt="" />
   </div>
 </footer>
